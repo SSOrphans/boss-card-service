0fc1cfc8236f42cbfbc02711db9e224026727b1d
Derrian Harris <80282599+DerrianH@users.noreply.github.com>
2021-05-05 22:04:34 -0500

    Feature/ssor 58 view cards on offer (#3)
    * Refactored Validation/ Test cases
    
    * SSOR-61 Refactored to use boss-core. Added swagger.
    
    * SSOR-58 Finished refactor
    
    * SSOR-58 Updated security and implemented h2 database
    
    * SSOR-58 Updated CardServiceTest
    
    * SSOR-58 Updated maven dependency to use Jackson XML data format. Added CORS @CrossOrigin annotation.
    
    Co-authored-by: Derrian Harris <dsh0018au@gmail.com>

---
+++ boss-card-service/src/main/java/org/ssor/boss/card/service/CardService.java
@@ -0,0 +1,78 @@
+/**
+ *
+ */
+package org.ssor.boss.card.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.ssor.boss.core.transfer.CardDto;
+import org.ssor.boss.core.entity.CardType;
+import org.ssor.boss.core.repository.CardRepository;
+import org.ssor.boss.core.entity.Card;
+
+import javassist.NotFoundException;
+
+/**
+ * @author Derrian Harris
+ *
+ */
+@Service
+public class CardService {
+	@Autowired
+	CardRepository cardDao;
+
+	public Card add(CardDto cardDto) throws IllegalArgumentException {
+
+		Card card = cardDto.convertToCardEntity();
+		card.setCreated(LocalDateTime.now());
+		return cardDao.save(card);
+	}
+
+	public Card update(CardDto cardDto) throws IllegalArgumentException, NotFoundException {
+		if (!cardDao.existsById(cardDto.getId()))
+			throw new NotFoundException("Resource not found with id: " + cardDto.getId());
+
+		Card card = cardDao.getOne(cardDto.getId());
+		card.setNumberHash(cardDto.getNumberHash());
+		card.setAccountId(cardDto.getAccountId());
+		card.setCreated(cardDto.getCreated());
+		card.setActiveSince(cardDto.getActiveSince());
+		card.setExpirationDate(cardDto.getExpirationDate());
+		card.setPin(cardDto.getPin());
+		card.setCvv(cardDto.getCvv());
+		card.setConfirmed(cardDto.getConfirmed());
+		card.setActive(cardDto.getActive());
+		card.setStolen(cardDto.getStolen());
+
+		return cardDao.save(card);
+	}
+
+	public Card findById(Integer id) throws IllegalArgumentException, NotFoundException {
+		Optional<Card> result = cardDao.findById(id);
+		if (result.isEmpty()) {
+			throw new NotFoundException("Resource not found with id: " + id);
+		}
+		return result.get();
+	}
+
+	public void deleteById(Integer id) throws IllegalArgumentException, NotFoundException {
+
+		if (!cardDao.existsById(id)) {
+			throw new NotFoundException("Resource not found with id: " + id);
+		}
+		cardDao.deleteById(id);
+	}
+
+	public List<Card> findAllCards() throws NotFoundException {
+		List<Card> cards = cardDao.findAll();
+		if (cards.isEmpty()) {
+			throw new NotFoundException("Resource not found");
+		}
+		return cards;
+	}
+
+}
---
+++ boss-card-service/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into boss.card (type_id, number_hash, account_id, created, active_since, expiration_date, pin, cvv, confirmed,
+                       active, stolen)
+values (0, '11111111111111111', 1, NOW(), NOW(), NOW(), 1111, 111, true, true, false);
+
+insert into boss.card (type_id, number_hash, account_id, created, active_since, expiration_date, pin, cvv, confirmed,
+                       active, stolen)
+values (0, '22222222222222222', 1, NOW(), NOW(), NOW(), 111, 111, true, true, false);
