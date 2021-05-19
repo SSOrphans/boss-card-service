06db5b262f46d5d70f10bf9e6199f8aa281a51a8
christianangeles <christian.angeles@smoothstack.com>
2021-05-19 10:34:17 -0700

    SSOR-99 update controller test for card detail ui

--- boss-card-service/src/test/java/org/ssor/boss/card/controller/CardControllerTest.java
+++ boss-card-service/src/test/java/org/ssor/boss/card/controller/CardControllerTest.java
@@ -4,6 +4,8 @@
 package org.ssor.boss.card.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,81 +34,93 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @SpringBootTest
 @AutoConfigureMockMvc
-public class CardControllerTest {
+public class CardControllerTest
+{
 
-    @Autowired
-    ObjectMapper mapper;
-    @Autowired
-    private MockMvc mvc;
+	@Autowired
+	ObjectMapper mapper;
+	@Autowired
+	private MockMvc mvc;
 
-    @MockBean
-    private CardService cardService;
+	@MockBean
+	private CardService cardService;
 
+	private Card cardA;
+	private Card cardE;
 
-    private Card cardA;
-    private Card cardE;
+	@BeforeEach
+	public void setup()
+	{
 
-    @BeforeEach
-    public void setup() {
+		LocalDateTime created = LocalDateTime.now();
+		LocalDate expirationDate = LocalDate.of(2025, 1, 1);
+		LocalDate activeSince = created.toLocalDate();
+		cardA = new Card();
+		cardE = new Card();
 
-        LocalDateTime created = LocalDateTime.now();
-        LocalDate expirationDate = LocalDate.of(2025, 1, 1);
-        LocalDate activeSince = created.toLocalDate();
-        cardA = new Card();
-        cardE = new Card();
+		cardA.setId(1);
+		cardA.setCardType(CardType.CARD_PLAIN);
+		cardA.setNumberHash("1234123412341234");
+		cardA.setAccountId(1);
+		cardA.setCreated(created);
+		cardA.setActiveSince(activeSince);
+		cardA.setExpirationDate(expirationDate);
+		cardA.setPin(1111);
+		cardA.setCvv(111);
+		cardA.setActive(false);
 
+		cardA.setConfirmed(false);
+		cardA.setStolen(false);
 
-        cardA.setId(1);
-        cardA.setCardType(CardType.CARD_PLAIN);
-        cardA.setNumberHash("1234123412341234");
-        cardA.setAccountId(1);
-        cardA.setCreated(created);
-        cardA.setActiveSince(activeSince);
-        cardA.setExpirationDate(expirationDate);
-        cardA.setPin(1111);
-        cardA.setCvv(111);
-        cardA.setActive(false);
+		cardE.setId(1);
+		cardE.setCardType(CardType.CARD_PLAIN);
+		cardE.setNumberHash("1234123412341234");
+		cardE.setAccountId(1);
+		cardE.setCreated(created);
+		cardE.setActiveSince(activeSince);
+		cardE.setExpirationDate(expirationDate);
+		cardE.setPin(1111);
+		cardE.setCvv(111);
+		cardE.setActive(false);
+		cardE.setConfirmed(false);
+		cardE.setStolen(false);
+	}
 
-        cardA.setConfirmed(false);
-        cardA.setStolen(false);
+	@Test
+	public void test_CanAddCard() throws Exception
+	{
+		when(cardService.add(cardA.convertToCardDto())).thenReturn(cardA);
 
-        cardE.setId(1);
-        cardE.setCardType(CardType.CARD_PLAIN);
-        cardE.setNumberHash("1234123412341234");
-        cardE.setAccountId(1);
-        cardE.setCreated(created);
-        cardE.setActiveSince(activeSince);
-        cardE.setExpirationDate(expirationDate);
-        cardE.setPin(1111);
-        cardE.setCvv(111);
-        cardE.setActive(false);
-        cardE.setConfirmed(false);
-        cardE.setStolen(false);
-    }
+		mvc.perform(post("/api/cards").contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(cardA.convertToCardDto()))).andExpect(status().isCreated())
+				.andExpect(content().json(mapper.writeValueAsString(cardE)));
+	}
 
+	@Test
+	public void test_CanUpdateCard() throws Exception
+	{
+		when(cardService.update(cardA.convertToCardDto())).thenReturn(cardA);
 
-    @Test
-    public void test_CanAddCard() throws Exception {
-        when(cardService.add(cardA.convertToCardDto())).thenReturn(cardA);
+		mvc.perform(put("/api/cards/1").contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(cardA.convertToCardDto()))).andExpect(status().isOk())
+				.andExpect(content().json(mapper.writeValueAsString(cardE)));
+	}
 
-        mvc.perform(post("/api/cards").contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(cardA.convertToCardDto()))).andExpect(status().isCreated())
-                .andExpect(content().json(mapper.writeValueAsString(cardE)));
-    }
+	@Test
+	public void test_CanDeleteCardById() throws Exception
+	{
+		doNothing().when(cardService).deleteById(any(Integer.class));
+		mvc.perform(
+				delete("/api/cards/1").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(cardA)))
+				.andExpect(status().isOk());
+	}
 
-    @Test
-    public void test_CanUpdateCard() throws Exception {
-        when(cardService.update(cardA.convertToCardDto())).thenReturn(cardA);
-
-        mvc.perform(put("/api/cards/1").contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(cardA.convertToCardDto()))).andExpect(status().isOk())
-                .andExpect(content().json(mapper.writeValueAsString(cardE)));
-    }
-
-    @Test
-    public void test_CanDeleteCardById() throws Exception {
-        doNothing().when(cardService).deleteById(any(Integer.class));
-        mvc.perform(delete("/api/cards/1").contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(cardA))).andExpect(status().isOk());
-    }
+	@Test
+	public void test_CanGetCardById() throws Exception
+	{
+		when(cardService.findById(cardA.getId())).thenReturn(cardA);
+		mvc.perform(
+				get("/api/cards/1").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(cardA)))
+				.andExpect(status().isOk());
+	}
 }
