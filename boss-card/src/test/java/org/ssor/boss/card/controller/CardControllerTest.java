5a249dbe272769cc2531ad48d1e33da9a445fdce
christianangeles <christian.angeles@smoothstack.com>
2021-05-20 19:26:20 -0700

    SSOR-223 update controller test for jacoco coverage test

--- boss-card/src/test/java/org/ssor/boss/card/controller/CardControllerTest.java
+++ boss-card/src/test/java/org/ssor/boss/card/controller/CardControllerTest.java
@@ -4,13 +4,17 @@
 package org.ssor.boss.card.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.ssor.boss.card.service.CardService;
 import org.ssor.boss.core.entity.Card;
@@ -18,9 +22,15 @@ import org.ssor.boss.core.entity.CardType;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import javassist.NotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -32,81 +42,189 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @SpringBootTest
 @AutoConfigureMockMvc
-class CardControllerTest {
+class CardControllerTest
+{
 
-    @Autowired
-    ObjectMapper mapper;
-    @Autowired
-    private MockMvc mvc;
+	@Autowired
+	ObjectMapper mapper;
+	@Autowired
+	private MockMvc mvc;
 
-    @MockBean
-    private CardService cardService;
+	@MockBean
+	private CardService cardService;
 
+	private Card cardA;
+	private Card cardE;
+	private MockHttpServletResponse mockResponse;
+	private String expectedContent;
 
-    private Card cardA;
-    private Card cardE;
+	@BeforeEach
+	void setup()
+	{
 
-    @BeforeEach
-    public void setup() {
+		LocalDateTime created = LocalDateTime.now();
+		LocalDate expirationDate = LocalDate.of(2025, 1, 1);
+		LocalDate activeSince = created.toLocalDate();
+		cardA = new Card();
+		cardE = new Card();
 
-        LocalDateTime created = LocalDateTime.now();
-        LocalDate expirationDate = LocalDate.of(2025, 1, 1);
-        LocalDate activeSince = created.toLocalDate();
-        cardA = new Card();
-        cardE = new Card();
+		cardA.setId(1);
+		cardA.setCardType(CardType.CARD_PLAIN);
+		cardA.setNumberHash("1234123412341234");
+		cardA.setAccountId(1);
+		cardA.setCreated(created);
+		cardA.setActiveSince(activeSince);
+		cardA.setExpirationDate(expirationDate);
+		cardA.setPin(1111);
+		cardA.setCvv(111);
+		cardA.setActive(false);
 
+		cardA.setConfirmed(false);
+		cardA.setStolen(false);
 
-        cardA.setId(1);
-        cardA.setCardType(CardType.CARD_PLAIN);
-        cardA.setNumberHash("1234123412341234");
-        cardA.setAccountId(1);
-        cardA.setCreated(created);
-        cardA.setActiveSince(activeSince);
-        cardA.setExpirationDate(expirationDate);
-        cardA.setPin(1111);
-        cardA.setCvv(111);
-        cardA.setActive(false);
+		cardE.setId(1);
+		cardE.setCardType(CardType.CARD_PLAIN);
+		cardE.setNumberHash("1234123412341234");
+		cardE.setAccountId(1);
+		cardE.setCreated(created);
+		cardE.setActiveSince(activeSince);
+		cardE.setExpirationDate(expirationDate);
+		cardE.setPin(1111);
+		cardE.setCvv(111);
+		cardE.setActive(false);
+		cardE.setConfirmed(false);
+		cardE.setStolen(false);
+	}
 
-        cardA.setConfirmed(false);
-        cardA.setStolen(false);
+	@AfterEach
+	void breakDown()
+	{
+		mockResponse = null;
+		expectedContent = null;
+	}
 
-        cardE.setId(1);
-        cardE.setCardType(CardType.CARD_PLAIN);
-        cardE.setNumberHash("1234123412341234");
-        cardE.setAccountId(1);
-        cardE.setCreated(created);
-        cardE.setActiveSince(activeSince);
-        cardE.setExpirationDate(expirationDate);
-        cardE.setPin(1111);
-        cardE.setCvv(111);
-        cardE.setActive(false);
-        cardE.setConfirmed(false);
-        cardE.setStolen(false);
-    }
+	@Test
+	void test_CanGetAllCards() throws Exception
+	{
+		List<Card> cardList = new ArrayList<>();
+		cardList.add(cardA);
+		cardList.add(cardE);
+		
+		expectedContent = mapper.writeValueAsString(cardList);
 
+		when(cardService.findAllCards()).thenReturn(cardList);
+		mockResponse = mvc.perform(get("/api/cards")).andReturn().getResponse();
 
-    @Test
-    void test_CanAddCard() throws Exception {
-        when(cardService.add(cardA.convertToCardDto())).thenReturn(cardA);
+		assertEquals(HttpStatus.OK.value(), mockResponse.getStatus());
+		assertEquals(expectedContent, mockResponse.getContentAsString());
+	}
 
-        mvc.perform(post("/api/cards").contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(cardA.convertToCardDto()))).andExpect(status().isCreated())
-                .andExpect(content().json(mapper.writeValueAsString(cardE)));
-    }
+	@Test
+	void test_CanGetAllCards_NotFound() throws Exception
+	{
+		when(cardService.findAllCards()).thenThrow(new NotFoundException("test"));
+		mockResponse = mvc.perform(get("/api/cards")).andReturn().getResponse();
 
-    @Test
-    void test_CanUpdateCard() throws Exception {
-        when(cardService.update(cardA.convertToCardDto())).thenReturn(cardA);
+		assertEquals(HttpStatus.NOT_FOUND.value(), mockResponse.getStatus());
+	}
 
-        mvc.perform(put("/api/cards/1").contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(cardA.convertToCardDto()))).andExpect(status().isOk())
-                .andExpect(content().json(mapper.writeValueAsString(cardE)));
-    }
+	@Test
+	void test_CanAddCard() throws Exception
+	{
+		when(cardService.add(cardA.convertToCardDto())).thenReturn(cardA);
 
-    @Test
-    void test_CanDeleteCardById() throws Exception {
-        doNothing().when(cardService).deleteById(any(Integer.class));
-        mvc.perform(delete("/api/cards/1").contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(cardA))).andExpect(status().isOk());
-    }
+		expectedContent = mapper.writeValueAsString(cardE);
+
+		mockResponse = mvc.perform(post("/api/cards").contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(cardA.convertToCardDto()))).andReturn().getResponse();
+
+		assertEquals(HttpStatus.CREATED.value(), mockResponse.getStatus());
+		assertEquals(expectedContent, mockResponse.getContentAsString());
+	}
+
+	@Test
+	void test_CanAddCard_BadRequest() throws Exception
+	{
+		when(cardService.add(cardA.convertToCardDto())).thenThrow(new IllegalArgumentException());
+
+		mockResponse = mvc.perform(post("/api/cards").contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(cardA.convertToCardDto()))).andReturn().getResponse();
+
+		assertEquals(HttpStatus.BAD_REQUEST.value(), mockResponse.getStatus());
+	}
+
+	@Test
+	void test_CanUpdateCard() throws Exception
+	{
+		when(cardService.update(cardA.convertToCardDto())).thenReturn(cardA);
+
+		expectedContent = mapper.writeValueAsString(cardE);
+
+		mockResponse = mvc.perform(put("/api/cards/1").contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(cardA.convertToCardDto()))).andReturn().getResponse();
+
+		assertEquals(HttpStatus.OK.value(), mockResponse.getStatus());
+		assertEquals(expectedContent, mockResponse.getContentAsString());
+	}
+
+	@Test
+	void test_CanUpdateCard_NotFound() throws Exception
+	{
+		when(cardService.update(cardA.convertToCardDto())).thenThrow(new NotFoundException("test"));
+
+		mockResponse = mvc.perform(put("/api/cards/1").contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(cardA.convertToCardDto()))).andReturn().getResponse();
+
+		assertEquals(HttpStatus.NOT_FOUND.value(), mockResponse.getStatus());
+	}
+
+	@Test
+	void test_CanUpdateCard_BadRequest() throws Exception
+	{
+		when(cardService.update(cardA.convertToCardDto())).thenThrow(new IllegalArgumentException());
+
+		mockResponse = mvc.perform(put("/api/cards/1").contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(cardA.convertToCardDto()))).andReturn().getResponse();
+
+		assertEquals(HttpStatus.BAD_REQUEST.value(), mockResponse.getStatus());
+	}
+
+	@Test
+	void test_CanDeleteCardById() throws Exception
+	{
+		doNothing().when(cardService).deleteById(any(Integer.class));
+
+		mockResponse = mvc
+				.perform(
+						delete("/api/cards/1").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(cardA)))
+				.andReturn().getResponse();
+
+		assertEquals(HttpStatus.OK.value(), mockResponse.getStatus());
+	}
+
+	@Test
+	void test_CanDeleteCardById_NotFound() throws Exception
+	{
+		doThrow(new NotFoundException("test")).when(cardService).deleteById(any(Integer.class));
+
+		mockResponse = mvc
+				.perform(
+						delete("/api/cards/1").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(cardA)))
+				.andReturn().getResponse();
+
+		assertEquals(HttpStatus.NOT_FOUND.value(), mockResponse.getStatus());
+	}
+
+	@Test
+	void test_CanDeleteCardById_BadRequest() throws Exception
+	{
+		doThrow(new IllegalArgumentException()).when(cardService).deleteById(any(Integer.class));
+
+		mockResponse = mvc
+				.perform(
+						delete("/api/cards/1").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(cardA)))
+				.andReturn().getResponse();
+
+		assertEquals(HttpStatus.BAD_REQUEST.value(), mockResponse.getStatus());
+	}
 }
